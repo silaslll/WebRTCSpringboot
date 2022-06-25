61a9dd4c676b914371e544610fa42b22352c4429
silaslll <lihangyi01@gmail.com>
2022-06-24 22:35:41 -0400

    test

--- webRTC+SpringBoot视频及文字聊天/demo/src/main/java/com/example/demo/web/socket/ServerRunner.java
+++ webRTC+SpringBoot视频及文字聊天/demo/src/main/java/com/example/demo/web/socket/ServerRunner.java
@@ -16,7 +16,6 @@ public class ServerRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        server.start();
-        
+        server.start();       
     }
 }
